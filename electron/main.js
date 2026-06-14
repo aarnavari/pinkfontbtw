6a3ed57832da14d748b05f80236f1816403c7c93
Aarna Vari <aarnavari@gmail.com>
2026-06-14 19:24:41 +0200

    Package desktop app with icon and static server

--- electron/main.js
+++ electron/main.js
@@ -1,8 +1,34 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const http = require("http");
+const handler = require("serve-handler");
 
 let mainWindow;
+let server;
 
-function createWindow() {
+function startStaticServer() {
+  return new Promise((resolve) => {
+    const outDir = path.join(__dirname, "../out");
+
+    server = http.createServer((request, response) => {
+      return handler(request, response, {
+        public: outDir,
+        rewrites: [
+          { source: "/stars/folder", destination: "/stars/folder/index.html" },
+          { source: "/stars", destination: "/stars/index.html" },
+          { source: "/", destination: "/index.html" },
+        ],
+      });
+    });
+
+    server.listen(0, "127.0.0.1", () => {
+      const address = server.address();
+      resolve(`http://127.0.0.1:${address.port}`);
+    });
+  });
+}
+
+async function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 850,
@@ -16,7 +42,12 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadURL("http://localhost:3000");
+  if (app.isPackaged) {
+    const url = await startStaticServer();
+    await mainWindow.loadURL(url);
+  } else {
+    await mainWindow.loadURL("http://localhost:3000");
+  }
 
   mainWindow.on("closed", () => {
     mainWindow = null;
@@ -26,6 +57,10 @@ function createWindow() {
 app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
+  if (server) {
+    server.close();
+  }
+
   if (process.platform !== "darwin") {
     app.quit();
   }
